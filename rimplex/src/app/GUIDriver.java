453022dfdf74bf0a141bf91b1c9cff99225729c4
Benjamin Huber <huberbp@dukes.jmu.edu>
2021-03-24 12:30:23 -0400

    Added very Rudimentary GUI,

---
+++ rimplex/src/app/GUIDriver.java
@@ -0,0 +1,62 @@
+package app;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+public class GUIDriver
+{
+  public static void main(String[] args) {
+    // WINDOW THINGS
+    JFrame window = new JFrame("Calculator");
+    window.setVisible(true);
+    window.setSize(360 ,150);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    // ALLOCATING WINDOW SPACE
+    JPanel calculatorDisplay = new JPanel();
+    window.getContentPane().add(BorderLayout.NORTH, calculatorDisplay);
+    JPanel buttonDisplay = new JPanel(new BasicOptionPaneUI.ButtonAreaLayout(true, 0));
+    window.getContentPane().add(BorderLayout.SOUTH, buttonDisplay);
+    // JPanel inputFieldDisplay = new JPanel(new BorderLayout());
+    // window.getContentPane().add(BorderLayout.CENTER, inputFieldDisplay);
+    
+    // CREATING OUR BUTTONS
+    JButton reset = new JButton("R");
+    JButton clear = new JButton("C");
+    JButton addition = new JButton("+");
+    JButton subtraction = new JButton("-");
+    JButton multiplication = new JButton("*");
+    JButton division = new JButton("/");
+    JButton equals = new JButton("=");
+    
+    // ADDING THEM TO LIST FOR EASIER INSTANTIATION AND MORE LEGIBLE CODE
+    List<JButton> buttonsList = new ArrayList<JButton>();
+    buttonsList.add(reset);
+    buttonsList.add(clear);
+    buttonsList.add(addition);
+    buttonsList.add(subtraction);
+    buttonsList.add(multiplication);
+    buttonsList.add(division);
+    buttonsList.add(equals);
+    
+    // ADDING THE BUTTONS TO THE PANEL
+    for(JButton alpha : buttonsList) {
+      alpha.setSize(400,400);
+      buttonDisplay.add(BorderLayout.CENTER, alpha);
+      
+      // alpha.setVisible(false);
+      // alpha.setVisible(true);
+    }
+    
+    // ADDING THE "DISPLAY" TO THE PANEL
+    JLabel display = new JLabel();
+    calculatorDisplay.add(display);
+    display.setText("DISPLAY");
+    
+    // ADDING THE INPUTFIELD TO THE PANEL
+    JTextField inputField = new JTextField();
+    window.add(BorderLayout.CENTER, inputField);
+  }
+}
