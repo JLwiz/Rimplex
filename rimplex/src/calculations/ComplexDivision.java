44163b8690ba942dc36da07ed149520dc0606b66
Ken <70919850+painteke@users.noreply.github.com>
2021-03-24 16:48:41 -0400

    Add ComplexSubtraction

--- rimplex/src/calculations/ComplexDivision.java
+++ rimplex/src/calculations/ComplexDivision.java
@@ -6,7 +6,17 @@ public class ComplexDivision implements Operations
   @Override
   public ComplexNumber calculate(ComplexNumber op1, ComplexNumber op2)
   {
-    // TODO Auto-generated method stub
+    if (op1 == null || op2 == null) {
+      // TODO: Raise some error
+      return null;
+    }
+    
+    Double op1real = op1.getReal();
+    Double op1imag = op1.getImaginary();
+    
+    Double op2real = op2.getReal();
+    Double op2imag = op2.getImaginary();
+    
     return null;
   }
 
--- rimplex/src/calculations/ComplexSubtraction.java
+++ rimplex/src/calculations/ComplexSubtraction.java
@@ -1,13 +1,41 @@
 package calculations;
 
+/**
+ * ComplexNumber addition class.
+ * 
+ * @author Kenneth Painter
+ * @version 03.24.2021
+ */
 public class ComplexSubtraction implements Operations
 {
 
+  /**
+   * Does ComplexNumber subtraction.
+   * 
+   * @param op1
+   *          ComplexNumber1
+   * @param op2
+   *          ComplexNumber2
+   * @return ComplexNumber post addition.
+   */
   @Override
   public ComplexNumber calculate(ComplexNumber op1, ComplexNumber op2)
   {
-    // TODO Auto-generated method stub
-    return null;
+    if (op1 == null || op2 == null) {
+      // TODO: Raise some error
+      return null;
+    }
+    
+    Double op1real = op1.getReal();
+    Double op1imag = op1.getImaginary();
+    
+    Double op2real = op2.getReal();
+    Double op2imag = op2.getImaginary();
+    
+    op2real *= -1;
+    op2imag *= -1;
+    
+    return new ComplexNumber(op1real - op2real, op1imag - op2imag);
   }
 
 }
