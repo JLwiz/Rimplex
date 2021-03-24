0a58d06a4f75cf823e91e237f3133fcd8e67bde7
Andrew Fryer <fryerak@dukes.jmu.edu>
2021-03-24 18:22:01 -0400

    Changed display a bit.

---
+++ rimplex/src/testing/EquationTest.java
@@ -0,0 +1,160 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class to test the Equation class.
+ * 
+ * @author Andrew Fryer
+ * @version 1.0 (03/24/2023)
+ */
+class EquationTest
+{
+ 
+  /**
+   * operandEmptyTest - Will test the operandEmpty method.
+   */
+  @Test
+  void operandEmptyTest()
+  {
+    
+  } // operandEmptyTest test.
+  
+  /**
+   * operatorEmptyTest - Will test the operatorEmpty method.
+   */
+  @Test
+  void operatorEmptyTest()
+  {
+    
+  } // operatorEmptyTest test.
+  
+  /**
+   * setOperandInvalidTests - Will test the setFirstOp and setSecondOp methods
+   * when the given parameter is invalid.
+   */
+  @Test
+  void setOperandInvalidTests()
+  {
+    
+  } // setOperandInvalidTests test.
+  
+  /**
+   * setOperandNullTests - Will test the setFirstOp and setSecondOp methods
+   * when the given parameter is null.
+   */
+  @Test
+  void setOperandNullTests()
+  {
+    
+  } // setOperandTests test.
+  
+  /**
+   * setOperandValidTests - Will test the setFirstOp and setSecondOp methods
+   * when the given parameter is valid.
+   */
+  @Test
+  void setOperandValidTests()
+  {
+    
+  } // setOperandValidTests test.
+  
+  /**
+   * setOperatorInvalidTest - Will test the setOperator method
+   * with an invalid parameter.
+   */
+  @Test
+  void setOperatorInvalidTest()
+  {
+    
+  } // setOperatorInvalidTest method.
+  
+  /**
+   * setOperatorNullTest - Will test the setOperator method
+   * with a null parameter.
+   */
+  @Test
+  void setOperatorNullTest()
+  {
+    
+  } // setOperatorNullTest test.
+  
+  /**
+   * setOperatorValidTest - Will test the setOperator method
+   * with a valid parameter.
+   */
+  @Test
+  void setOperatorValidTest()
+  {
+    
+  } // setOperatorValidTest test.
+  
+  /**
+   * singletonTest - Will make sure that there is only one 
+   * instance of the Equation.
+   */
+  @Test
+  void singletonTest()
+  {
+    
+  } // singletonTest test.
+  
+  /**
+   * solveAdditionTest - Will test the solve method with addition.
+   */
+  @Test
+  void solveAdditionTest()
+  {
+    
+  } // solveAdditionTest test.
+  
+  /**
+   * solveBadInputsTest - Will test that the solve method correctly
+   * handles bad inputs.
+   */
+  @Test
+  void solveBadInputsTest()
+  {
+    
+  } // solveBadInputsTest test.
+  
+  /**
+   * solveDivisionTest - Will test the solve method with division.
+   */
+  @Test
+  void solveDivisionTest()
+  {
+    
+  } // solveDivisionTest test.
+  
+  /**
+   * solveMultiplicationTest - Will test the solve method with multiplication.
+   */
+  @Test
+  void solveMultiplicationTest()
+  {
+    
+  } // solveMultiplicationTest test.
+  
+  /**
+   * solveSetsTest - Will test that the solve method correctly sets
+   * operands and operator after solving.
+   */
+  @Test
+  void solveSetsTest()
+  {
+    
+  } // solveSetsTest test.
+  
+  /**
+   * solveSubtractionTest - Will test the solve method with subtraction.
+   */
+  @Test
+  void solveSubtractionTest()
+  {
+    
+  } // solveSubtractionTest test.
+  
+} // EquationTest class.
