41182f76bba10c1d7a6f1843f582c3f9d9a70566
Jacob Lewis <lewisjw@dukes.jmu.edu>
2021-03-24 15:10:32 -0400

    Added random testing for complex multiplication.

--- rimplex/src/calculations/ComplexMultiplication.java
+++ rimplex/src/calculations/ComplexMultiplication.java
@@ -26,20 +26,15 @@ public class ComplexMultiplication implements Operations
    */
   public ComplexNumber calculate(final ComplexNumber op1, final ComplexNumber op2)
   {
-
-    Double realOne = 0.0;
-    Double realTwo = 0.0;
-    Double imgOne = 0.0;
-    Double imgTwo = 0.0;
+    Double newReal = 0.0;
+    Double newImg = 0.0;
     if (op1 != null && op2 != null)
     {
-      realOne = op1.getReal() * op2.getReal();
-      realTwo = op1.getReal() * op2.getImaginary();
-      imgOne = op1.getImaginary() * op2.getReal();
-      imgTwo = op1.getImaginary() * op2.getImaginary() * -1;
+      newReal += op1.getReal() * op2.getReal();
+      newReal += op1.getImaginary() * op2.getImaginary() * -1;
+      newImg += op1.getReal() * op2.getImaginary();
+      newImg += op1.getImaginary() * op2.getReal();
     } 
-    Double newReal = realOne + imgTwo;
-    Double newImg = realTwo + imgOne;
 
     return new ComplexNumber(newReal, newImg);
   }
--- rimplex/src/testing/ComplexMultiplicationTest.java
+++ rimplex/src/testing/ComplexMultiplicationTest.java
@@ -6,23 +6,62 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * @author Jacob Lewis
+ * @version 03.24.2021
+ */
 class ComplexMultiplicationTest
 {
 
   @Test
-  void multiCalculateTest()
+  void multiplicationCalculateTest()
   {
-    Operations op = new ComplexMultiplication();
-    ComplexNumber op1 = new ComplexNumber(0.0, 2.0);
-    ComplexNumber op2 = new ComplexNumber(3.0, -8.0);
-    ComplexNumber expected = new ComplexNumber(16.0, 6.0);
-    ComplexNumber actual = op.calculate(op1, op2);
-    assertEquals(actual.getReal(), expected.getReal());
-    assertEquals(actual.getImaginary(), expected.getImaginary());
+    Operations operator = new ComplexMultiplication();
+    ComplexNumber op1;
+    ComplexNumber op2;
+    ComplexNumber expected;
+    ComplexNumber actual;
+
+    op1 = new ComplexNumber(0.0, 2.0);
+    op2 = new ComplexNumber(3.0, -8.0);
+    expected = new ComplexNumber(16.0, 6.0);
+    actual = operator.calculate(op1, op2);
+    testHelper(actual, expected);
+
+    op1 = new ComplexNumber(0.0, 0.0);
+    op2 = new ComplexNumber(3.0, -8.0);
+    expected = new ComplexNumber(0.0, 0.0);
+    actual = operator.calculate(op1, op2);
+    testHelper(actual, expected);
+
+    op1 = new ComplexNumber(0.0, 2.0);
+    op2 = new ComplexNumber(3.0, 8.0);
+    expected = new ComplexNumber(-16.0, 6.0);
+    actual = operator.calculate(op1, op2);
+    testHelper(actual, expected);
   }
-  
+
+  @Test
+  void multiplicationRandomTesting()
+  {
+    Double[] values = new Double[4];
+    for (int i = 0; i < values.length; i++)
+    {
+      values[i] = 100 * Math.random();
+    }
+    ComplexNumber op1 = new ComplexNumber(values[0], values[1]);
+    ComplexNumber op2 = new ComplexNumber(values[2], values[3]);
+    Double real = (values[0] * values[2]) + (values[1] * values[3] * -1);
+    Double img = (values[0] * values[3]) + (values[1] * values[2]);
+    Operations operator = new ComplexMultiplication();
+    ComplexNumber expected = new ComplexNumber(real, img);
+    ComplexNumber actual = operator.calculate(op1, op2);
+    testHelper(actual, expected);
+  }
+
   /**
    * General testHelper for to test the new calculated ComplexNumber.
+   * 
    * @param actual
    * @param expected
    */
