5f41eb9fe237717124854c61d97039154f439a2a
Jacob Lewis <lewisjw@dukes.jmu.edu>
2021-03-24 12:06:10 -0400

    Implemented the Operations interface along with ComplexAdd class for complex addition.

---
+++ rimplex/src/calculations/ComplexAdd.java
@@ -0,0 +1,13 @@
+package calculations;
+
+import util.ComplexNumber;
+
+public class ComplexAdd implements Operations {
+
+  @Override
+  public ComplexNumber calculate(ComplexNumber real, ComplexNumber img) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ rimplex/src/calculations/Operations.java
@@ -0,0 +1,12 @@
+package calculations;
+import util.ComplexNumber;
+
+/**
+ * @author Jacob Lewis
+ * @version 03.24.2021
+ */
+public interface Operations {
+  
+  public ComplexNumber calculate(ComplexNumber real, ComplexNumber img);
+
+}
