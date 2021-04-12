f4eea8188167a7e27c647b0ef6dab88c06719a42
Benjamin Huber <huberbp@dukes.jmu.edu>
2021-04-12 19:05:33 -0400

    Fixed some NullPointerException.

--- rimplex/src/GUI/ButtonPadPanel.java
+++ rimplex/src/GUI/ButtonPadPanel.java
@@ -28,7 +28,7 @@ public class ButtonPadPanel extends JPanel
   
   private HashMap<String, JButton> buttonMap;
   
-  private JButton modeButton = createButton("DEC", "mode");
+  private JButton modeButton;
   
   /**
    * The default constructor for our ButtonPadPanel.
@@ -104,7 +104,7 @@ public class ButtonPadPanel extends JPanel
     
     numpad.gridx = leftEdge + 1;
     numpad.gridy = topEdge;
-    
+    JButton modeButton = createButton("DEC", "mode");
     add(modeButton, numpad);
     
     numpad.gridy = topEdge + 1;
