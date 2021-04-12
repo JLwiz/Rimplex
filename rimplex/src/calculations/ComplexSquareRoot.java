5c9161be66b0736f3263c4dcb696dfe0cfbbc750
Ken <70919850+painteke@users.noreply.github.com>
2021-04-12 15:02:23 -0400

    Add exponentiation, partial square root, partial graphing, and tests

---
+++ rimplex/src/calculations/ComplexSquareRoot.java
@@ -0,0 +1,36 @@
+package calculations;
+
+/**
+ * ComplexNumber SquareRoot class.
+ * 
+ * @author Kenneth Painter
+ * @version 03.24.2021
+ */
+public class ComplexSquareRoot implements Operations
+{
+  @Override
+  public ComplexNumber calculate(final ComplexNumber... op) 
+  {
+    ComplexNumber answer = new ComplexNumber(0.0, 0.0);
+    ComplexNumber op1;
+    ComplexNumber op2;
+    if (op != null && op.length == 2) 
+    {
+      op1 = op[0];
+      op2 = op[1];
+    } else 
+    {
+      return answer;
+    }
+    
+    if (op1 == null || op2 == null)
+    {
+      // TODO: Raise some error
+      return null;
+    }
+
+    Double op1real = op1.getReal();
+    return new ComplexNumber(Math.sqrt(op1real), 0.0);
+  }
+
+}
