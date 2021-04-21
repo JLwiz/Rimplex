90de830515f4b4ad00b37448a504ff73ddb49ba3
Benjamin Huber <huberbp@dukes.jmu.edu>
2021-04-21 15:54:22 -0400

    Added extra buttons row.

--- rimplex/src/GUI/ButtonPadPanel.java
+++ rimplex/src/GUI/ButtonPadPanel.java
@@ -122,6 +122,27 @@ public class ButtonPadPanel extends JPanel
     numpad.gridy = topEdge + 4;
     JButton sqrtButton = createButton("sqrt", "squareroot");
     add(sqrtButton, numpad);
+    
+    numpad.gridx = leftEdge + 2;
+    numpad.gridy = topEdge;
+    JButton sinButton = createButton("sin", "sin");
+    add(sinButton, numpad);
+    
+    numpad.gridy = topEdge + 1;
+    JButton cosButton = createButton("cos", "cos");
+    add(cosButton, numpad);
+    
+    numpad.gridy = topEdge + 2;
+    JButton tanButton = createButton("tan", "tan");
+    add(tanButton, numpad);
+    
+    numpad.gridy = topEdge + 3;
+    JButton realPartButton = createButton("RP", "realpart");
+    add(realPartButton, numpad);
+    
+    numpad.gridy = topEdge + 4;
+    JButton imaginaryPartButton = createButton("IP", "imaginarypart");
+    add(imaginaryPartButton, numpad);
   }
   /**
    * Creates and adds the "Number pad" to the JPanel. Should be noted that the number pad takes up 3
@@ -250,7 +271,7 @@ public class ButtonPadPanel extends JPanel
     add(clearButton, numpad);
     
     numpad.gridx = leftEdge + 2;
-    JButton backspaceButton = createButton("<-", "backspace");
+    JButton backspaceButton = createButton("<html>&#8592</html>", "backspace");
     add(backspaceButton, numpad);
   }
 
