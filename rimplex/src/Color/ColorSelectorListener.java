26a53c18c32e6fc15ab52634941d71addf1649e5
Benjamin Huber <huberbp@dukes.jmu.edu>
2021-04-26 12:52:17 -0400

    Implemented ColorSelector writing to file.  TODO: Loading on startup.

--- rimplex/src/Color/ColorSelectorListener.java
+++ rimplex/src/Color/ColorSelectorListener.java
@@ -2,15 +2,31 @@ package Color;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ColorSelectorListener implements ActionListener
 {
-
+  private ColorSelector color;
   @Override
   public void actionPerformed(ActionEvent e)
   {
-    // TODO
-
+    color = ColorSelector.getInstance();
+    
+    int fcolors[] = color.getForegroundColors();
+    int bcolors[] = color.getBackgroundColors();
+    
+    try
+    {
+      FileWriter colorWriter = new FileWriter("src/app/config.txt");
+      colorWriter.write(fcolors[0] + " " + fcolors[1] + " " + fcolors[2] + "\n");
+      colorWriter.write(bcolors[0] + " " + bcolors[1] + " " + bcolors[2] + "\n");
+      colorWriter.close();
+    }
+    catch (IOException e1)
+    {
+      System.out.println("Could not find config file.");
+    }
   }
 
 }
