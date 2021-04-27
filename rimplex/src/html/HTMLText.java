d8567e9cf6e7a57769ee00f5f543f73b971212cb
Andrew Fryer <fryerak@dukes.jmu.edu>
2021-04-27 16:42:59 -0400

    HTML about page.

---
+++ rimplex/src/html/HTMLText.java
@@ -0,0 +1,27 @@
+package html;
+
+/**
+ * HTMLText - Holds text for html.
+ * 
+ * @author Andrew Fryer
+ * @version 1.0 (04/27/2021)
+ */
+public final class HTMLText
+{
+  public static final String ABOUTPAGE = "<html>\r\n"
+      + "<title> Rimplex : About </title>\r\n"
+      + "  <body>\r\n"
+      + "    <img src=\"resources/images/logoRimplex.png\" alt=\"rimplex logo\" width=\"200\" height= \"100\">"
+      + "    <p>"
+      + "      <h4>Rimplex is a complex calculator that allows users to solve mathamatical equations that<br>"
+      + "      involve imaginary numbers. Users can also perform an abundance of operations including sin, cos,<br>"
+      + "      tan, conjugate, log and other standard operators. Rimplex also includes functionality to allow<br>"
+      + "      users to see their previous calculations and record/playback their calculations.</h4><br>"
+      + "      <h5>Product Owner: David Bernstein</h5>"
+      + "      <h5>Contributors: Storm Behrens, Andrew Fryer, Ben Huber, Jacob Lewis, Kenneth Painter</h5>"
+      + "      <h6>Version: 2021-04 (3.0)</h6>"
+      + "      <h6>(c) Copyright Rimplex contributers 2021. All rights reserved.</h6>"
+      + "    </p>"
+      + "  </body>"
+      + "</html>";
+} // HTMLText class
